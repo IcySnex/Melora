61a3da58e47c31a3abff0ba405152bf500a273d1
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-04-13 13:31:06 +0200

    Designed Home View

--- Musify/ViewModels/HomeViewModel.cs
+++ Musify/ViewModels/HomeViewModel.cs
@@ -1,17 +1,51 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml.Controls;
+using Musify.Views;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.System;
 
 namespace Musify.ViewModels;
 
 public partial class HomeViewModel : ObservableObject
 {
     readonly ILogger<HomeViewModel> logger;
+    readonly MainView mainView;
 
     public HomeViewModel(
-        ILogger<HomeViewModel> logger)
+        ILogger<HomeViewModel> logger,
+        MainView mainView)
     {
         this.logger = logger;
+        this.mainView = mainView;
 
         logger.LogInformation("[HomeViewModel-.ctor] HomeViewModel has been initialized");
+    }
+
+
+    [RelayCommand]
+    async Task OpenBrowserAsync(
+        string url)
+    {
+        if (await mainView.AlertAsync($"Do you want to open your default browser with the url '{url}'?", "Are you sure?", "No", "Yes") != ContentDialogResult.Primary)
+            return;
+
+        await Launcher.LaunchUriAsync(new(url));
+        logger.LogInformation("[HomeViewModel-OpenBrowserAsync] Browser was opened with url: {url}", url);
+    }
+
+    [RelayCommand]
+    async Task CopyToClipboardAsync(
+        string text)
+    {
+        if (await mainView.AlertAsync($"Do you want to copy the text '{text}' to your clipboard?", "Are you sure?", "No", "Yes") != ContentDialogResult.Primary)
+            return;
+
+        DataPackage data = new();
+        data.SetText(text);
+
+        Clipboard.SetContent(data);
+        logger.LogInformation("[HomeViewModel-ShareAsync] Clipboard was set to text: {text}", text);
     }
 }
