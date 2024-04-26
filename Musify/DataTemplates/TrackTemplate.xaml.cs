14861fd317e797d6c1f3cf1b07caf1572d197946
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-04-26 22:44:25 +0200

    Removed unused template

--- Musify/DataTemplates/TrackTemplate.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.UI.Xaml;
-
-namespace Musify.DataTemplates;
-
-public partial class TrackTemplate : ResourceDictionary
-{
-    public TrackTemplate() =>
-        InitializeComponent();
-}
