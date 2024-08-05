ea22ebb0002fbef4c6c7381279c3fecb2dafbab5
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-08-05 20:57:30 +0200

    Renamed IntOverZeroVisibilityConverter to IntIsZeroVisibilityConverter

--- Musify/Converters/IntIsZeroVisibilityConverter.cs
+++ Musify/Converters/IntIsZeroVisibilityConverter.cs
@@ -3,10 +3,10 @@ using Microsoft.UI.Xaml.Data;
 
 namespace Musify.Converters;
 
-public class IntOverZeroVisibilityConverter : IValueConverter
+public class IntIsZeroVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, string language) =>
-        (int)value > 0 ? Visibility.Collapsed : Visibility.Visible;
+        (int)value == 0 ? Visibility.Visible : Visibility.Collapsed;
 
     public object ConvertBack(object value, Type targetType, object parameter, string language) =>
         throw new NotImplementedException();
