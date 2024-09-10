54942bebfb7f13fa90f14d91a9c690ad71d439be
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-09-10 19:49:57 +0200

    Performance & memory usage improvements

--- Melora/Helpers/Extensions.cs
+++ Melora/Helpers/Extensions.cs
@@ -40,10 +40,19 @@ public static class Extensions
     }
 
 
+    static readonly char[] invalidFileNameChars =
+        [
+            '\"', '<', '>', '|', '\0',
+            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+            (char)31, ':', '*', '?', '/'
+        ];
+
     public static string ToLegitFileName(
         this string input)
     {
-        foreach (char c in Path.GetInvalidFileNameChars().Except(['\\']))
+        foreach (char c in invalidFileNameChars)
             input = input.Replace(c, '_');
 
         return input;
